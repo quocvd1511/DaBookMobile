93e13d6e2e73516c6df6f0c7e42c93d69c66f53d
ThuyTrang2906 <19522384@gm.uit.edu.vn>
2021-12-14 01:19:40 +0700

    ignore

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './source/cart/cart';
-import App from './App';
+import App from './source/book_detail/book_detail';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
