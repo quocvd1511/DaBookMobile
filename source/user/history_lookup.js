10b7e4b44288a9bbe0b526877605b0af3b02e8c0
ThuyTrang2906 <19522384@gm.uit.edu.vn>
2021-12-17 23:16:57 +0700

    chi tiet don hang

--- source/user/history_lookup.js
+++ source/user/history_lookup.js
@@ -6,6 +6,30 @@ export default function HistoryLookup()
     return(
         <ScrollView>
             <View style={{paddingTop: 20}}>
+                <View style={{backgroundColor:'#f0f8ff', borderRadius:5, borderWidth: 1, borderColor: '#1ba8ff', margin:5,}}>
+                    <Text style={styles.text_style}>Mã đơn hàng:  DH11</Text>
+                    <Text style={styles.text_style}>Ngày đặt hàng:  17/12/2021</Text>
+                    <Text style={styles.text_style}>Địa chỉ người nhận:</Text>
+                        <View style={{backgroundColor:'#f0f8ff'}}>
+                            <View style={styles.main}>
+                                <View style={styles.type_user}>
+                                    <Image style={styles.icon_style} source={require('../asset/icon/location.png')}/>
+                                    <Text style={{fontSize: 16}}>Hồng Nguyễn</Text>
+                                </View>
+
+                                <View style={styles.type_numberphone}>
+                                    <Text style={{fontSize: 16}}>SĐT: 0123456777</Text>
+                                </View>
+                            </View>
+                            <View style={styles.address}>
+                                <Text style={{fontSize: 16}}>Địa chỉ: phường An phú, thị xã Thuận An, tỉnh Bình Dương</Text>
+                            </View>
+                        </View>
+                    <View style={styles.deli_dabook}>
+                        <Text style={styles.text_style}>Được giao bởi: </Text>
+                        <Image style={styles.deli_style} source={require('../asset/icon/dabook_deli.png')}/>
+                    </View>
+                </View>
                 <View style = {styles.view1}>
                     <Image style = {styles.img_book} source={{uri:'https://vcdn.tikicdn.com/media/catalog/product/c/o/cover%20thanh%20xuan%20de%20danh.u2487.d20160905.t152805.232712.jpg'}}/>    
                     <View>
@@ -18,10 +42,10 @@ export default function HistoryLookup()
                     </View>
                 </View>
                 <View style = {styles.view1}>
-                    <Image style = {styles.img_book} source={{uri:'https://designs.vn/wp-content/images/18-04-2013/Beyond%20the%20cover%20Ng%C3%B4n%20t%C3%ACnh%20%20(14).jpg'}}/>    
+                    <Image style = {styles.img_book} source={{uri:'https://inthienhang.com/wp-content/uploads/2020/03/bia-sach-co-font-chu-dep-278x400.jpg'}}/>    
                     <View>
                         <Text style={styles.book_name} numberOfLines={2}
-                            ellipsizeMode='tail'>Nếu anh là truyền thuyết của em</Text>
+                            ellipsizeMode='tail'>Nếu anh là truyền thuyết của em đối thoại với ma đối thoại với người</Text>
                         <View style={{display: 'flex', flexDirection: 'row', marginTop: 10, marginRight:10,}}>
                             <Text style = {styles.price}> 110.000 đ </Text>
                             <Text style={styles.newprice}>101.000 đ</Text>
@@ -56,36 +80,6 @@ export default function HistoryLookup()
                     </Pressable>
                 </View>
             </View>
-
-            <View style={{paddingTop: 20}}>
-                <View style = {styles.view1}>
-                    <Image style = {styles.img_book} source={{uri:'https://inthienhang.com/wp-content/uploads/2020/03/bia-sach-co-font-chu-dep-278x400.jpg'}}/>    
-                    <View>
-                        <Text style={styles.book_name} numberOfLines={2}
-                            ellipsizeMode='tail'>Đối thoại cùng ma Đối thoại cùng ma Đối thoại cùng ma Đối thoại cùng ma Đối thoại cùng ma</Text>
-                        <View style={{display: 'flex', flexDirection: 'row', marginTop: 10, marginRight:10,}}>
-                            <Text style = {styles.price}> 197.000 đ </Text>
-                            <Text style={styles.newprice}>158.000 đ</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style = {styles.view2}>
-                    <Text style={{fontSize: 16, color: 'blue'}}>1 sản phẩm</Text>
-                    <Text style={{fontSize: 16}}>Thành tiền:<Text style={styles.newprice}> 178.000 đ</Text></Text>
-                </View>
-                <View style = {styles.view3}>
-                    <Text style={{fontSize: 17, color: 'green'}}>Giao hàng thành công</Text>
-                    <Pressable 
-                        backgroundColor={'#FF6600'}
-                        width={100}
-                        height={34}
-                        marginBottom={-5}
-                        marginTop={-5}
-                        borderRadius={5}>
-                        <Text style={{color:'#fff', fontSize: 17, margin: 5, textAlign:'center'}} >Mua lại</Text>
-                    </Pressable>
-                </View>
-            </View>
         </ScrollView>
     )
 }
@@ -93,6 +87,76 @@ export default function HistoryLookup()
 
 const styles=StyleSheet.create({
     
+    main:{
+        backgroundColor: '#f0f8ff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    deli_dabook:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'space-between',
+    },
+
+    address:{
+        backgroundColor: '#f0f8ff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 30,
+        paddingRight: 20,
+        marginBottom: 10,
+    },
+
+    type_user:{
+        flexDirection: 'row',
+        height: 50,
+        width:'50%',
+        justifyContent:'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+
+    type_numberphone:{
+        flexDirection: 'row',
+        height: 50,
+        width:'50%',
+        justifyContent:'center',
+        alignItems: 'center',
+        marginRight:10,
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+
+    icon_style:
+    {
+        height: 25,
+        width: 25,
+        marginRight: 10,
+        marginLeft: 15,
+        tintColor: '#1E90FF'
+    },
+
+    deli_style: {
+        resizeMode: "contain",
+        height: 90,
+        width: 110,
+        marginRight: 70,
+        marginLeft: 15,
+        paddingRight:30,
+    },
+
+    text_style:{
+        fontSize: 17,
+        tintColor: '#1E90FF',
+        fontWeight: '600',
+        lineHeight: 28,
+        marginLeft: 10,
+        color:'#444'
+    },
+
     card:{
         marginTop: 15,
         height: 120,
