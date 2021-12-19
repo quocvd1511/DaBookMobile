2acfc7c5509dcfa46595af48866cfad3e8e5f5f0
ThuyTrang2906 <19522384@gm.uit.edu.vn>
2021-12-19 08:54:47 +0700

    pull

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './source/category/category';pp from './App';
-import App from './source/user/history_lookup';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
